70fc7c78fed9ad91cfe8cca42f8f55fa969867ba
David <36444115+dawissl@users.noreply.github.com>
2021-05-03 18:12:18 +0200

    init commit

---
+++ Revision/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        class Clovek
+        {
+            public int vek;
+            private string jmeno;
+            private string prijmeni;
+
+            public string Jmeno { get { return jmeno; } }
+            public string Prijmeni { set { prijmeni = value; } }
+        }
+    }
+}

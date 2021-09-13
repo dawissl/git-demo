ed54d084d7666efeba784cabe61da4a853d02560
David <dawis.sl@gmail.com>
2021-09-13 11:12:40 +0200

    feature version 1

--- Revision/Program.cs
+++ Revision/Program.cs
@@ -11,6 +11,7 @@ namespace Revision
         static void Main(string[] args)
         {
             Console.WriteLine("Code change");
+            Console.WriteLine("New feature");
         }
 
         class Clovek
